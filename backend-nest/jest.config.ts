453c4c80afa8deea37eb4d41762d4dfb204ddb8d
SmNikc <NNSmetanin@gmail.com>
2025-09-06 12:04:03 +0300

    Update jest.config.ts

--- backend-nest/jest.config.ts
+++ backend-nest/jest.config.ts
@@ -1,11 +1,17 @@
 import type { Config } from 'jest';
+
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/test', '<rootDir>/src'],
+  roots: ['<rootDir>/test'], // держим тесты только здесь
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   transform: { '^.+\\.(t|j)sx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }] },
   transformIgnorePatterns: ['/node_modules/'],
-  testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  testMatch: ['**/test/**/*.spec.(ts|tsx|js)'], // единый шаблон
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'], // не трогаем сборку
+  // (опционально) покрытие:
+  // collectCoverageFrom: ['src/**/*.{ts,tsx,js}'],
+  // coverageDirectory: 'coverage'
 };
+
 export default config;
